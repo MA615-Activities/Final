e230a4d4fa6d6ea21c82bf4a7a3cde0cfacdcd39
maysenpagan <mppagan@bu.edu>
2023-12-14 12:00:54 -0500

    SWOT analysis completed

--- ui.R
+++ ui.R
@@ -6,6 +6,7 @@ library(shinydashboard)
 library(shinydashboardPlus)
 library(leaflet)
 library(shinythemes)
+library(shinyBS)
 
 
 
@@ -34,7 +35,7 @@ ui <- navbarPage(id = "tabs",
                                      h1(style="font-family: 'Georgia', serif;", "Fun Facts"),
                                      tags$ul(
                                        tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", "Since a substantial amount of 
-                                       evenue is generated from oil and gas reserves, Brunei is one of 16 countries without income tax."), 
+                                       revenue is generated from oil and gas reserves, Brunei is one of 16 countries without income tax."), 
                                        tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", "The Istana Nurul Iman, 
                                                the Sultan's official residence, holds the Guinness World Record as the largest residential palace
                                                in the world."), 
@@ -94,37 +95,114 @@ ui <- navbarPage(id = "tabs",
                  nav_panel("SWOT", p("SWOT Analysis",
                                      panelsPage(
                                        panel(title = "Strengths",
+                                                       style = "success",
                                              collapsed = TRUE,
                                              width = 300,
-                                             body = h1("Nothing here")
+                                             body = div(h3(style="font-family: 'Georgia', serif;",
+                                                           "Economic Stability"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Brunei is rich is oil and natural gas preserves which provides
+                                                                  a stable income for the country.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Standard of Living"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Citizens in Brunei have a high standard of living as a result
+                                                                  of low crime rates combined with high education, subsidized healthcare, 
+                                                                  and many social welfare programs.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Location"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Brunei's geographical location in Southeast Asia provides
+                                                                  strategic access to sea trade routes.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Culture"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "The strong influence of Malay and Islamic culture and
+                                                                  traditions attracts many tourists.")
+                                                          )
+                                                        )
                                              ),
                                        panel(title = "Weaknesses",
                                              collapsed = TRUE,
                                              width = 300,
-                                             body = div(h3("Here is some info"),
-                                                        hr(),
-                                                        verbatimTextOutput("debug"),
-                                                        hr(),
-                                                        p("Lorem ipsum dolor sit amet, consectetur adipisicing elit, 
-                                                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                                                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                                                        nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in 
-                                                        reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-                                                        nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                                                        sunt in culpa qui officia deserunt mollit anim id est laborum.")
-                                                        ),
-                                             hr(),
-                                             h2("More info")
+                                             body = div(h3(style="font-family: 'Georgia', serif;",
+                                                           "Economic Dependence"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Brunei is heavily reliant on oil and gas exports which makes
+                                                                  the economy vulnerable to changes in global oil prices. This
+                                                                  dependence also hinders diversification as it hinders the
+                                                                  development of other sectors.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Limited Workforce"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Brunei's relatively small population leads to a limited
+                                                                  workforce and talent pool for economic development.")
+                                                          )
+                                                        )
                                              ),
                                        panel(title = "Opportunities",
                                              width = 350,
                                              collapsed = TRUE,
-                                             body = div(plotOutput("plot"))
+                                             body = div(h3(style="font-family: 'Georgia', serif;",
+                                                           "Diversification"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Opportunities exist for diversifying the economy
+                                                                  into sectors like tourism, technology, and renewable energy
+                                                                  to reduce dependency on oil and natural gases.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Infrastructure"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Investing in the development and improvement of 
+                                                                  Brunei's infrastructure could attract foreign investments
+                                                                  and enhance economic growth.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Regional Collaboration"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Opportunities exist for trade and investment by Brunei's
+                                                                  participation in regional economic alliances and partnerships.")
+                                                          )
+                                                        )
                                              ),
                                        panel(title = "Threats",
                                              collapsed = TRUE,
-                                             body = div(),
-                                             footer = HTML("Footer")
+                                             body = div(h3(style="font-family: 'Georgia', serif;",
+                                                           "Global Economic Changes"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Changes in the global prices of oil as well as economic downturns
+                                                                  can harm Brunei's economy.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Environmental Threats"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Factors like deforestation, pollution, and loss of biodiversity
+                                                                  threaten Brunei's natural resources and ecosystems.")
+                                                          ),
+                                                        h3(style="font-family: 'Georgia', serif;",
+                                                           "Competition"),
+                                                        tags$ul(
+                                                          tags$li(style="font-size:13pt; font-family: 'Georgia', serif;", 
+                                                                  "Brunei's growth is also threatened by competition from
+                                                                  neighboring countries for investments, tourism, and a skilled
+                                                                  workforce.")
+                                                          )
+                                                        )
                                              )
                                        )
                                      )
